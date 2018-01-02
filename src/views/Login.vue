33dccfd28d2df5ced4a9cadda6828b936a605d29
yangyuan <846158012@qq.com>
2018-01-02 18:05:57 +0800

    登录页面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,54 @@
 <template>
-    <div id="login">
-        <div class="login-container">
-            <h3 class="title">共享点餐商家端</h3>
-            <el-form :model="ruleForm" ref="ruleForm" label-width="60px">
-                <el-form-item prop="account" label="手机号">
-                    <el-input type="text" v-model="ruleForm.sellerName" auto-complete="off" placeholder="用户名" :maxlength="11"></el-input>
-                </el-form-item>
-                <el-form-item prop="checkPass" label="验证码">
-                    <el-input class="code-input" type="text" v-model="ruleForm.code" auto-complete="off" placeholder="验证码" :maxlength="4"></el-input>
-                    <el-button type="primary" class="get-code-btn" @click="getCode" :disabled="isClickGetCode">获取验证码</el-button>
-                </el-form-item>
-                <el-form-item style="width:100%;">
-                    <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit" :loading="logining">登录</el-button>
-                </el-form-item>
-                <p class="copyright">鑫圆共享电子商务股份有限公司© 2017
-                    <br>蜀ICP备17032496号-4</p>
-            </el-form>
+    <div>
+        <el-row class="header">
+            <el-col class="logo">
+                <el-col :span="10">
+                    <router-link to="/index" class="router-link">
+                        <img src="../assets/images/logo1.png" alt="logo" class="logo-icon">
+                        <span class="txt">手机验证登录</span>
+                    </router-link>
+                </el-col>
+                <el-col :span="7" class="userinfo">
+                </el-col>
+            </el-col>
+        </el-row>
+        <div id="login">
+            <div class="login-container">
+                <h3 class="title">共享点餐商家端</h3>
+                <el-form :model="ruleForm" ref="ruleForm" label-width="60px">
+                    <el-form-item prop="account" label="手机号">
+                        <el-input type="text" v-model="ruleForm.sellerName" auto-complete="off" placeholder="用户名" :maxlength="11"></el-input>
+                    </el-form-item>
+                    <el-form-item prop="checkPass" label="验证码">
+                        <el-input class="code-input" type="text" v-model="ruleForm.code" auto-complete="off" placeholder="验证码" :maxlength="4"></el-input>
+                        <el-button type="success" class="get-code-btn" @click="getCode" :disabled="isClickGetCode">获取验证码</el-button>
+                    </el-form-item>
+                    <el-form-item style="width:100%;">
+                        <el-button type="success" style="width:100%;" @click.native.prevent="handleSubmit" :loading="logining">登录</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
         </div>
+        <el-row class="content">
+            <el-col :span="9" :offset="5">
+                <h4>产品简介</h4>
+                <p>共享点餐商家端，是共享点餐推出的商家管理系统。用户端处理订单，管理订单，管理菜品，商品。赶快入住共享点餐吧，让赚钱变得更简单。共享点餐商家端，是共享点餐推出的商家管理系统。用客户端处理订单，管理订单，管理菜品，商品。赶快入出共享点餐，让赚钱变得更简单。</p>
+            </el-col>
+            <el-col :span="8" :offset="2">
+                <h4>联系我们</h4>
+                <p class="p-margin">电话:022-88888888</p>
+                <p class="p-margin">邮箱:xinyuangongxiang@888tal.com</p>
+                <p class="p-margin">四川省成都市高新区通威国际中心19楼</p>
+            </el-col>
+        </el-row>
+        <el-row class="footer-bar">
+            <p>鑫圆共享电子商务股份有限公司©2017 &nbsp; 蜀ICP备17032496号-4 <br><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51019002001128" class="beian-link">
+                <img src="../assets/images/beian-gov.png"/>川公网安备 51019002001128号</a>
+            </p>
+        </el-row>
     </div>
+    
+
 </template>
 <script>
  import {
@@ -93,43 +124,132 @@ export default {
     }
 }
 </script>
-<style lang="scss" scoped>
+<style scoped>
 #login{
     width: 100%;
-    height: 100%;
-    position: absolute;
-    //background-color: #ff0000;
+    height: 650px;
+    position: relative;
+    background-image: url("../assets/images/login.png");
+    background-repeat: no-repeat;
 }
 .login-container {
     -webkit-border-radius: 5px;
     border-radius: 5px;
     -moz-border-radius: 5px;
     background-clip: padding-box;
-    margin: 15% 0 0 60%;
-    ;
     background-color: #eee;
     width: 290px;
     padding: 15px 35px;
-    .title {
-        font-size: 20px;
-        margin: 10px auto;
-        text-align: center;
-        color: #363f44;
-    }
-    .code-input{
-        width: 45%;
-    }
-    .get-code-btn{
-        display: inline-block;
-        width: 50%;
-    }
-    .remember {
-        margin-bottom: 20px;
-    }
-    .copyright {
-        font-size: 14px;
-        color: #98a6ad;
-        text-align: center;
-    }
+    position: absolute;
+    margin:15% 0 0 60%;
+}
+.title {
+    font-size: 20px;
+    margin: 10px auto;
+    text-align: center;
+    color: #363f44;
+}
+.code-input{
+    width: 45%;
+}
+.get-code-btn{
+    display: inline-block;
+    width: 50%;
+}
+.remember {
+    margin-bottom: 20px;
+}
+.copyright {
+    font-size: 14px;
+    color: #98a6ad;
+    text-align: center;
+}
+.footer-bar{
+    width: 100%;
+    padding-top: 30px;
+    padding-bottom: auto;
+    text-align: center;
+    background-color: #fff;
+}
+.footer-bar p{
+    color: #333;
+    line-height: 1.5;
+}
+.beian-link{
+    text-decoration: none;
+    color: #333;
+}
+.beian-link img{
+    vertical-align: middle;
+}
+.header {
+    height: 60px;
+    line-height: 60px;
+    color: #666;
+    background-color: #fff; 
+}
+.logo {
+    width: 1200px;
+    height: 60px;
+    position: absolute;
+    left: 50%;
+    margin-left: -600px;
+    font-size: 0; 
+}
+.avator{
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    vertical-align: middle;
+    cursor: pointer;
+}
+.logo-width {
+    width: 231px;
+}
+.logo-collapse-width {
+    width: 60px;
+    padding: 0;
+    text-align: center;
+}
+.logo-icon{
+    width: 54px;
+    vertical-align: middle;
+}
+.router-link {
+    padding: 0;
+}
+.txt {
+    color: #666;
+    font-size: 16px;
+    margin-left: 18px;
+    border-left: 2px solid #666;
+    padding-left: 15px;
+    vertical-align: middle;
+}
+.router-link {
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+    text-align: center;
+    color: #666;
+    text-decoration: none;
+}
+.tools {
+    padding: 0px 23px;
+    width: 14px;
+    height: 60px;
+    line-height: 60px;
+    cursor: pointer;
+}
+.p-margin{
+    margin: 0
+}
+.content{
+    background-color: #fff
 }
 </style>
+
+
+
