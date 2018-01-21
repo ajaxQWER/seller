fbbddb2e771b1a0bbefde5efbd486343e2365bea
yangyuan <846158012@qq.com>
2018-01-21 09:59:45 +0800

    修改配置

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL: '"//seller.gongxiangdiancan.com"',
+  BASE_URL: '"//api.gongxiangdiancan.com"',
   UPLOAD_URL: '"//uploads.gongxiangdiancan.com"'
 }
--- config/test.env.js
+++ config/test.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production-test"',
-  BASE_URL: '"//seller.test.gongxiangdiancan.com"',
+  BASE_URL: '"//api.test.gongxiangdiancan.com"',
   UPLOAD_URL: '"//uploads.test.gongxiangdiancan.com"'
 }
